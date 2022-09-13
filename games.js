6205398bd64b4f7d620a9ccd17faeaf60e903910
Daniel Medina <daniel@medinaarmada.com>
2022-09-13 13:34:51 -0400

    Refactor del mapa de juego

--- games.js
+++ games.js
@@ -16,27 +16,28 @@ function setCanvasSize() {
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
 
-  elementSize = (canvasSize / 10);
+  elementSize = canvasSize / 10;
 
   startGame();
 }
 
 function startGame() {
   console.log({ canvasSize, elementSize });
-  
+
   game.font = elementSize + "px Verdana";
   game.textAlign = "end";
 
   const map = maps[0];
   const mapRows = map.trim().split("\n");
-  const mapRowCols = mapRows.map( row => row.trim().split(''));
-  console.log({map, mapRows, mapRowCols});
+  const mapRowCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows, mapRowCols });
 
-
-
-  for (let row = 1; row <= 10; row++) {
-    for (let col = 1; col <=10; col++){
-      game.fillText(emojis[mapRowCols[row-1][col-1]], elementSize * col, elementSize * row);
-    }
-  }
+  mapRowCols.forEach((row, rowI) => {
+    row.forEach((col, colI) => {
+      const emoji = emojis[col];
+      const posX = elementSize * (colI + 1);
+      const posY = elementSize * (rowI + 1);
+      game.fillText(emoji, posX,posY);
+    });
+  });
 }
