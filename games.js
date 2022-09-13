5258a712150d3640d7c00774ab013b64afc1fcdc
Daniel Medina <daniel@medinaarmada.com>
2022-09-13 12:43:04 -0400

    ¿Qué es un array bidimensional?

--- games.js
+++ games.js
@@ -16,16 +16,27 @@ function setCanvasSize() {
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
 
-  elementSize = canvasSize / 10;
+  elementSize = (canvasSize / 10);
 
   startGame();
 }
 
 function startGame() {
   console.log({ canvasSize, elementSize });
+  
   game.font = elementSize + "px Verdana";
   game.textAlign = "end";
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis["X"], elementSize * i, elementSize);
+
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowCols = mapRows.map( row => row.trim().split(''));
+  console.log({map, mapRows, mapRowCols});
+
+
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <=10; col++){
+      game.fillText(emojis[mapRowCols[row-1][col-1]], elementSize * col, elementSize * row);
+    }
   }
 }
